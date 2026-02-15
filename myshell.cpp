3461148587f48c32b906a501de1e60d40fe4cfbb
ali-albakri <alialbakri304@gmail.com>
2026-02-15 06:21:26 +0200

    Added all required internal commands

--- myshell.cpp
+++ myshell.cpp
@@ -3,6 +3,9 @@
 #include <sys/wait.h>
 #include <string>
 #include <cstring>
+#include <cstdlib>
+#include <dirent.h>
+#include <vector>
 
 using namespace std;
 
@@ -13,20 +16,7 @@ int main() {
         cout << "theshell>";
         getline(cin, cmd);
 
-        if (cmd == "quit") {
-            cout << "Exiting..." << endl;
-            break;
-        }
-
-        pid_t pid = fork();
-
-        if (pid < 0){
-            perror("Forking Error");
-            return 1;
-        }
-
-        if (pid == 0) {
-            char ccmd[256];
+        char ccmd[256];
             strncpy(ccmd, cmd.c_str(), sizeof(ccmd));
             ccmd[sizeof(ccmd)-1] = '\0';
 
@@ -39,6 +29,91 @@ int main() {
             }
             args[x] = nullptr;
 
+        // Internal Shell commands
+        if (cmd == "quit") {
+            cout << "Exiting..." << endl;
+            break;
+        }  else if (strcmp(args[0], "help") == 0) {
+            cout << "Simple CS shell commands:" << endl
+                 << "cd X            | print current X or change current directory" << endl
+                 << "dir X           | list directory X's contents" << endl
+                 << "environ         | list environment variables" << endl
+                 << "set X Y         | set value Y of variable X" << endl
+                 << "echo X          | prints X" << endl
+                 << "pause           | pause and wait for Enter" << endl
+                 << "quit            | quits shell" << endl
+                 << "Note: Other external commands work." << endl;
+            continue; }
+            else if (strcmp(args[0], "cd") == 0) {
+                if (args[1] == nullptr) {
+                char cwd[256];
+                getcwd(cwd, sizeof(cwd));
+                cout << cwd << endl;
+            } else {
+                if (chdir(args[1]) != 0) {
+                    perror("CD Error");
+                } else {
+                    setenv("PWD", args[1], 1);
+                }
+            }
+            continue;
+        } else if (strcmp(args[0], "dir") == 0) {
+            const char *pathdir;
+            if (args[1] != nullptr) {
+                pathdir = args[1];  // use the directory the user typed
+            } else {
+                pathdir = ".";       // default to current directory
+            }
+
+            DIR *dir = opendir(pathdir);
+            if (!dir) {
+                perror("DIR Error");
+                continue;
+            }
+
+            struct dirent *direntry;
+            while ((direntry = readdir(dir)) != nullptr) {
+                  cout << direntry->d_name << endl;
+            }
+            cout << endl;
+            closedir(dir);
+            continue;
+        } else if (strcmp(args[0], "pause") == 0) {
+            cout << "Press Enter to continue..." << flush;
+            while (cin.get() != '\n'); 
+            continue;
+        } else if (strcmp(args[0], "echo") == 0) {
+            for (int j = 1; args[j] != nullptr; j++) {
+                cout << args[j];
+                if (args[j+1] != nullptr) {
+                     cout << " ";
+                }
+            }
+            cout << endl;
+            continue;
+        } else if (strcmp(args[0], "environ") == 0) {
+            extern char **environ;
+            for (char **env = environ; *env != 0; env++) {
+                cout << *env << endl;
+            }
+            continue;
+        } else if (strcmp(args[0], "set") == 0) {
+            if (args[1] && args[2]) {
+                setenv(args[1], args[2], 1);
+            } else {
+                cout << "set (Variable) (Value)" << endl;
+            }
+            continue;
+        } 
+
+        pid_t pid = fork();
+
+        if (pid < 0){
+            perror("Forking Error");
+            return 1;
+        }
+        
+        if (pid == 0) {
             execvp(args[0], args);
             perror("Execvp Failed");
         } else {
