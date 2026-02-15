0be12cd667e9cc1ab7653e296bb84022817ff234
ali-albakri <alialbakri304@gmail.com>
2026-02-15 04:39:09 +0200

    Added the fork/exec shell

--- myshell.cpp
+++ myshell.cpp
@@ -1,6 +1,48 @@
 #include <iostream>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string>
+#include <cstring>
+
 using namespace std;
 
 int main() {
-    cout << "the shell works :P" << endl;
+    string cmd;
+
+    while (true) {
+        cout << "theshell>";
+        getline(cin, cmd);
+
+        if (cmd == "quit") {
+            cout << "Exiting..." << endl;
+            break;
+        }
+
+        pid_t pid = fork();
+
+        if (pid < 0){
+            perror("Forking Error");
+            return 1;
+        }
+
+        if (pid == 0) {
+            char ccmd[256];
+            strncpy(ccmd, cmd.c_str(), sizeof(ccmd));
+            ccmd[sizeof(ccmd)-1] = '\0';
+
+            char *args[32];
+            int x = 0;
+            char *part = strtok(ccmd, " ");
+            while (part != nullptr && x < 63) {
+                args[x++] = part;
+                part = strtok(nullptr, " ");
+            }
+            args[x] = nullptr;
+
+            execvp(args[0], args);
+            perror("Execvp Failed");
+        } else {
+            wait(NULL);
+        }
+    }
 }
